e253bfe0d5827a0a7b9740d9867e987964396a76
bekmurzaevse <bekmurzaevse@gmail.com>
2025-04-25 12:29:50 +0500

    configured redis and cache

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    Cache::remember('test', 600, function () {
+        return "kfjdslkjfksdfjsklfjsklfjklsdjfksdfjsklf";
+    });
     return view('welcome');
 });
