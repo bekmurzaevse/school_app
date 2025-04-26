bfb715577b395d25326b6ae0acf874860117fde5
jalalatdin7413 <ramanovjalalatdin@gmail.com>
2025-04-26 13:08:49 +0500

    fix: method casts and added softdeletes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
@@ -21,5 +22,10 @@ class Category extends Model
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
         ];
+    }
+
+    public function documents()
+    {
+        return $this->hasMany(Document::class);
     }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,15 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
         'description',
         'start_time',
+        'school_id',
     ];
 
     protected function casts(): array
@@ -21,6 +23,7 @@ class Event extends Model
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
+            'start_time' => 'datetime',
         ];
     }
     public function school()
