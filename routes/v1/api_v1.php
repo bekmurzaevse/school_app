e81b55db5a3e764e0c5567dae8820bfae107739d
bekmurzaevse <bekmurzaevse@gmail.com>
2025-04-30 11:26:18 +0500

    School CRUD and Exception created

--- routes/v1/api_v1.php
+++ routes/v1/api_v1.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Controllers\v1\SchoolController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Category;
 
+Route::pattern('id', '\d+');
+Route::pattern('hash', '[a-z0-9]+');
+Route::pattern('hex', '[a-f0-9]+');
+Route::pattern('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
+Route::pattern('base', '[a-zA-Z0-9]+');
+Route::pattern('slug', '[a-z0-9-]+');
+Route::pattern('username', '[a-z0-9_-]{3,16}');
+
 Route::get('/', function () {
     return Category::first()->name;
+});
+
+Route::prefix('schools')->group(function () {
+    Route::get('/', [SchoolController::class, 'index']);
+    Route::get('/{id}', [SchoolController::class, 'show']);
+    Route::post('/create', [SchoolController::class, 'create']);
+    Route::put('/update/{id}', [SchoolController::class, 'update']);
+    Route::delete('/delete/{id}', [SchoolController::class, 'delete']);
+
 });
