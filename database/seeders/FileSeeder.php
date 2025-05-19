9a65324dcd6c4d30afc33cd121f55a004f9ee3db
jalalatdin7413 <ramanovjalalatdin@gmail.com>
2025-05-19 10:45:54 +0500

    fix: changes fileseeder

--- database/seeders/FileSeeder.php
+++ database/seeders/FileSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\File;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class FileSeeder extends Seeder
 {
@@ -12,43 +13,56 @@ class FileSeeder extends Seeder
      */
     public function run(): void
     {
-        File::create([
-            'name' => [
-                'en' => 'Science Project Report',
-                'ru' => 'Отчет о научном проекте',
-                'uz' => 'Fan loyihasi hisobot',
-                'kk' => "Pa'n joybarı esabatı",
+        $files = [
+            [
+                'filename' => 'science_project_report.pdf',
+                'name' => [
+                    'en' => 'Science Project Report',
+                    'ru' => 'Отчет о научном проекте',
+                    'uz' => 'Fan loyihasi hisobot',
+                    'kk' => "Pa'n joybarı esabatı",
+                ],
+                'description' => [
+                    'en' => 'Report for the science project of the school science fair',
+                    'ru' => 'Отчет о научном проекте для школьной научной ярмарки',
+                    'uz' => 'Maktab fan ko‘rgazmasi uchun fan loyihasi hisobot',
+                    'kk' => "Mektep pa'n ko'rgizbesi ushın pa'n joybarı esabatı",
+                ],
+                'event_id' => 1,
+                'content' => 'This is a sample content for the science project report.',
             ],
-            'description' => [
-                'en' => 'Report for the science project of the school science fair',
-                'ru' => 'Отчет о научном проекте для школьной научной ярмарки',
-                'uz' => 'Maktab fan ko‘rgazmasi uchun fan loyihasi hisobot',
-                'kk' => "Mektep pa'n ko'rgizbesi ushın pa'n joybarı esabatı",
+            [
+                'filename' => 'football_tournament_results.pdf',
+                'name' => [
+                    'en' => 'Football Tournament Results',
+                    'ru' => 'Результаты футбольного турнира',
+                    'uz' => 'Futbol turniri natijalari',
+                    'kk' => "Futbol jarisi na'tiyjeleri",
+                ],
+                'description' => [
+                    'en' => 'Results of the inter-school football competition',
+                    'ru' => 'Результаты межшкольного футбольного соревнования',
+                    'uz' => 'Maktablararo futbol musobaqasi natijalari',
+                    'kk' => "Mektepler ara futbol jarısı na'tiyjeleri",
+                ],
+                'event_id' => 2,
+                'content' => 'Sample results of the football tournament.',
             ],
-            'event_id' => 1, 
-            'path' => 'uploads/science_project_report.pdf',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        ];
 
-        File::create([
-            'name' => [
-                'en' => 'Football Tournament Results',
-                'ru' => 'Результаты футбольного турнира',
-                'uz' => 'Futbol turniri natijalari',
-                'kk' => "Futbol jarisi na'tiyjeleri",
-            ],
-            'description' => [
-                'en' => 'Results of the inter-school football competition',
-                'ru' => 'Результаты межшкольного футбольного соревнования',
-                'uz' => 'Maktablararo futbol musobaqasi natijalari',
-                'kk' => "Mektepler ara futbol jarısı na'tiyjeleri",
-            ],
-            'event_id' => 2, 
-            'path' => 'uploads/football_tournament_results.pdf',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
-        
+        foreach ($files as $file) {
+            $path = 'uploads/' . $file['filename'];
+
+            Storage::disk('public')->put($path, $file['content']);
+
+            File::create([
+                'name' => $file['name'],
+                'description' => $file['description'],
+                'event_id' => $file['event_id'],
+                'path' => $path,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
