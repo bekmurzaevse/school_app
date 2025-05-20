06faf01e44a9212a289586d5cc49a0a1dfd633b8
bekmurzaevse <bekmurzaevse@gmail.com>
2025-05-20 10:35:23 +0500

    feat: Created swagger for AlbumController

---
+++ app/Http/Controllers/Swagger/v1/SchoolController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Swagger\v1;
+
+use App\Http\Controllers\Controller;
+
+class SchoolController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function show()
+    {
+        //
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function delete()
+    {
+        //
+    }
+}
