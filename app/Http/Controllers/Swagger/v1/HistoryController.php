a7c080a3d7a903832ea5c491de1f5bd5ff9e1030
jalalatdin7413 <ramanovjalalatdin@gmail.com>
2025-05-26 22:45:04 +0500

    added test and swagger Target History

---
+++ app/Http/Controllers/Swagger/v1/HistoryController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers\Swagger\v1;
+use OpenApi\Attributes as OA;
+
+use App\Http\Controllers\Controller;
+
+class HistoryController extends Controller
+{
+    #[OA\Get(
+        path: '/api/v1/histories',
+        description: "All histories",
+        tags: ["History"],
+        summary: "All histories",
+    )]
+    #[OA\Response(response: 200, description: 'History list')]
+    #[OA\Response(response: 401, description: 'Not allowed')]
+    public function index()
+    {
+        //
+    }
+
+    #[OA\Get(path: '/api/v1/histories/{id}', summary: "History by id", tags: ["History"])]
+    #[OA\Parameter(
+        name: "id",
+        in: "path",
+        required: true,
+        description: "ID arqali history aliw",
+        example: 1
+    )]
+    #[OA\Response(response: 200, description: 'History')]
+    #[OA\Response(response: 401, description: 'Unauthenticated')]
+    public function show()
+    {
+        //
+    }
+
+    #[OA\Post(
+        path: '/api/v1/histories/create',
+        summary: 'History jaratiw',
+        description: "History jaratiw ushin",
+        tags: ['History'],
+        security: [['sanctum' => []]]
+    )]
+    #[OA\RequestBody(
+        required: true,
+        description: "History jaratiw ushin mag'liwmatlar",
+        content: new OA\JsonContent(
+            required: ["year", "school_id", "text"],
+            properties: [
+                new OA\Property(property: "school_id", type: "integer", example: 1),
+                new OA\Property(property: "year", type: "integer", example: 2024),
+                new OA\Property(
+                    property: "text",
+                    type: "object",
+                    required: ["kk", "uz", "ru", "en"],
+                    properties: [
+                        new OA\Property(property: "kk", type: "string", example: "kk history text"),
+                        new OA\Property(property: "uz", type: "string", example: "uz history text"),
+                        new OA\Property(property: "ru", type: "string", example: "ru history text"),
+                        new OA\Property(property: "en", type: "string", example: "en history text"),
+                    ]
+                ),
+            ]
+        )
+    )]
+    #[OA\Response(response: 200, description: 'History created!')]
+    #[OA\Response(response: 401, description: 'Not allowed')]
+    public function create()
+    {
+        //
+    }
+
+    #[OA\Put(
+        path: "/api/v1/histories/update/{id}",
+        summary: "History jan'alaw",
+        tags: ["History"],
+        security: [["sanctum" => []]],
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(
+                required: ["year", "school_id", "text"],
+                properties: [
+                    new OA\Property(property: "school_id", type: "integer", example: 1),
+                    new OA\Property(property: "year", type: "integer", example: 2025),
+                    new OA\Property(
+                        property: "text",
+                        type: "object",
+                        required: ["kk", "uz", "ru", "en"],
+                        properties: [
+                            new OA\Property(property: "kk", type: "string", example: "kk updated text"),
+                            new OA\Property(property: "uz", type: "string", example: "uz updated text"),
+                            new OA\Property(property: "ru", type: "string", example: "ru updated text"),
+                            new OA\Property(property: "en", type: "string", example: "en updated text"),
+                        ]
+                    ),
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(response: 200, description: "History updated!"),
+            new OA\Response(response: 401, description: "Not allowed"),
+            new OA\Response(response: 404, description: "History not found!")
+        ]
+    )]
+    #[OA\Parameter(
+        name: "id",
+        in: "path",
+        required: true,
+        description: "ID arqali history jan'law",
+        example: 1
+    )]
+    public function update()
+    {
+        //
+    }
+
+    #[OA\Delete(
+        path: "/api/v1/histories/delete/{id}",
+        summary: "History o'shiriw",
+        tags: ["History"],
+        security: [["sanctum" => []]],
+        parameters: [
+            new OA\Parameter(name: "id", in: "path", required: true, schema: new OA\Schema(type: "integer"))
+        ],
+        responses: [
+            new OA\Response(response: 200, description: "History deleted!"),
+            new OA\Response(response: 401, description: "Not allowed"),
+            new OA\Response(response: 404, description: "History not found!"),
+        ]
+    )]
+    public function delete()
+    {
+        //
+    }
+}
---
+++ tests/Feature/HistoryTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\History;
+use App\Models\School;
+use App\Models\Target;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class HistoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+    public function test_history_can_get_all(): void
+    {
+        $response = $this->getJson('api/v1/histories');
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'status',
+                'message',
+                'data' => [
+                    'items',
+                    'pagination' => [
+                        'current_page',
+                        'per_page',
+                        'last_page',
+                        'total',
+                    ]
+                ]
+            ]);
+    }
+
+    public function test_history_can_show(): void
+    {
+        $history = History::inRandomOrder()->first();
+
+        $response = $this->getJson("/api/v1/histories/{$history->id}");
+
+        $response
+            ->assertOk()
+            ->assertJsonStructure([
+                'status',
+                'message',
+                'data'
+            ]);
+    }
+
+    public function test_history_can_create(): void
+    {
+        $user = User::find(1)->first();
+        $school = School::inRandomOrder()->first();
+
+        $this->actingAs($user);
+
+        $data = [
+            'school_id' => $school->id,
+            'year' => 2025,
+            'text' => [
+                'en' => 'Plan for 2025',
+                'ru' => 'План на 2025',
+                'uz' => '2025 yil uchun reja',
+                'kk' => '2025 jilga reje',
+            ],
+        ];
+
+        $response = $this->postJson('/api/v1/histories/create', $data);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+                'message' => 'History created!',
+            ]);
+    }
+
+    public function test_history_can_update(): void
+    {
+        $user = User::find(1)->first();
+        $this->actingAs($user);
+
+        $history = History::inRandomOrder()->first();
+
+        $updatedData = [
+            'school_id' => $history->school_id,
+            'year' => 2026,
+            'text' => [
+                'en' => 'Updated plan',
+                'ru' => 'Обновлённый план',
+                'uz' => 'Yangilangan reja',
+                'kk' => 'Janalangan reje',
+            ],
+        ];
+
+        $response = $this->putJson("/api/v1/histories/update/{$history->id}", $updatedData);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+                'message' => 'History updated!',
+            ]);
+    }
+
+    public function test_history_can_delete(): void
+    {
+        $user = User::find(1)->first();
+        $this->actingAs($user);
+
+        $history = History::inRandomOrder()->first();
+
+        $response = $this->deleteJson("/api/v1/histories/delete/{$history->id}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+                'message' => "{$history->id} - History deleted!",
+            ]);
+    }
+}
