472193c5fe763979118aab17616ed9e5dd2c971a
bekmurzaevse <bekmurzaevse@gmail.com>
2025-06-04 11:03:00 +0500

    fix:Album update

--- app/Actions/v1/Albums/UpdateAction.php
+++ app/Actions/v1/Albums/UpdateAction.php
@@ -4,6 +4,7 @@ namespace App\Actions\v1\Albums;
 
 use App\Dto\v1\Albums\UpdateDto;
 use App\Exceptions\ApiResponseException;
+use App\Helpers\FileUploadHelper;
 use App\Http\Resources\v1\Album\AlbumResource;
 use App\Models\Album;
 use App\Traits\ResponseTrait;
@@ -29,6 +30,27 @@ class UpdateAction
                 'title' => $dto->title,
                 'description' => $dto->description
             ]);
+
+            if ($dto->photos) {
+                $existingPhotoNames = $album->photos->pluck('name')->toArray();
+
+                foreach ($dto->photos as $photo) {
+                    $originalName = $photo->getClientOriginalName();
+
+                    if (in_array($originalName, $existingPhotoNames)) {
+                        continue;
+                    }
+
+                    $path = FileUploadHelper::file($photo, 'photo');
+
+                    $album->photos()->create([
+                        'name' => $originalName,
+                        'path' => $path,
+                        'type' => 'photo',
+                        'size' => $photo->getSize(),
+                    ]);
+                }
+            }
 
             return static::toResponse(
                 message: "ALbom jan'alandi!",
